3f7a90ed1b3417679612ed81066f9e4ecce2a777
Anastasia Kravchenko <anastasia.kravchenko@sibers.com>
2022-11-13 22:23:00 +0600

    initial commit

---
+++ NailIt/NailIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NailIt
+//
+//  Created by Anastasia Kravchenko on 13.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
